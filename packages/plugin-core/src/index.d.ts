ffe050a48aea43b99d9d9657acbc20f75bbb87b6
zkz098 <zkz20081204@126.com>
2026-08-26 20:34:54 +0800

    chore: remove stale .d.ts from src/ and add gitignore rule

--- packages/plugin-core/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "./manifest";
-export * from "./config";
-export * from "./registry";
--- packages/plugin-core/src/manifest.d.ts
+++
@@ -1,7 +0,0 @@
-import { type CustomElementEntry, type HyacineArticleContext, type HyacinePluginSystemConfig, type HydrationInstruction, type InjectEntry, type InjectPointDetail, type InjectPointsConfig, type InjectPointValue, type InjectPosition, type PluginManifest, type PluginPlatformType, type RenderCapability, type RuntimeOnlyEntry, type SSREntry, type StandardSlotName } from "@hyacine/contract";
-export type { CustomElementEntry, HyacineArticleContext, HyacinePluginSystemConfig, HydrationInstruction, InjectEntry, InjectPointDetail, InjectPointsConfig, InjectPointValue, InjectPosition, PluginManifest, PluginPlatformType, RenderCapability, RuntimeOnlyEntry, SSREntry, StandardSlotName, };
-export type PluginFunction<TOptions = any> = (options: TOptions) => PluginManifest;
-/**
- * 声明并校验一个 Hyacine 插件 Manifest
- */
-export declare function definePlugin(manifest: PluginManifest): PluginManifest;
